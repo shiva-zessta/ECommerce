aea4e477d352337f21c45a256ddcda289fea1463
shiva <90833050+shiva-zessta@users.noreply.github.com>
2025-09-03 16:50:02 +0530

    integrated postgres (#2)

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ECommerce.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
